2c897ab7bde3b556466aad0469689f05d9a3cac7
markiu200 <markiu2007@interia.pl>
2025-03-01 16:57:30 +0100

    Test out how strings work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Simplest app')));
+  const String name = 'smok';
+  final String color = 'white';
+  const lastName = 'Kasmok';
+  final alignment = 'evil';
+  // https://api.flutter.dev/flutter/widgets/Text-class.html
+  // https://api.flutter.dev/flutter/dart-core/String-class.html
+  runApp(MaterialApp(home: Text('$name $lastName is $color and $alignment')));
 }
