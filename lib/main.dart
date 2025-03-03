3fc9faab66a9093555f3d24cf28875d79c87f98e
markiu200 <markiu2007@interia.pl>
2025-03-03 02:28:38 +0100

    Add scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,9 @@ void main() {
   final alignment = 'evil';
   // https://api.flutter.dev/flutter/widgets/Text-class.html
   // https://api.flutter.dev/flutter/dart-core/String-class.html
-  runApp(MaterialApp(home: Text('$name $lastName is $color and $alignment')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: Text('$name $lastName is $color and $alignment')),
+    ),
+  );
 }
